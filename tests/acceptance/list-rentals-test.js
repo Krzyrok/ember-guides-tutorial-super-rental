bc12f8b7ff05537907fb9f0562ac2ff3ead5081f
Krzyrok <krzyrok@gmail.com>
2017-08-20 12:33:42 +0200

    add tests for main page, contact page and about page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,21 +3,28 @@ import moduleForAcceptance from 'ember-guides-tutorial-super-rental/tests/helper
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('should show rentals as the home page', function(assert) {
-
+test('should show rentals as the home page', (assert) => {
+  visit('/');
+  andThen(() => assert.equal(currentURL(), '/rentals', 'should redirect automatically'));
 });
 
-test('should link to information about the company.', function (assert) {
+test('should link to information about the company.', (assert) => {
+  visit('/');
+  click('a:contains("About")');
+  andThen(() => assert.equal(currentURL(), '/about', 'should navigate to about'));
 });
 
-test('should link to contact information.', function (assert) {
+test('should link to contact information.', (assert) => {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(() => assert.equal(currentURL(), '/contact', 'should navigate to contact'));
 });
 
-test('should list available rentals.', function (assert) {
+test('should list available rentals.', (assert) => {
 });
 
-test('should filter the list of rentals by city.', function (assert) {
+test('should filter the list of rentals by city.', (assert) => {
 });
 
-test('should show details for a selected rental', function (assert) {
+test('should show details for a selected rental', (assert) => {
 });
